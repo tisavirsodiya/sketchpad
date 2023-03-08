47364d92af0dc1eb5b7cd53a02280170b9bb0a57
tisa virsodiya <virsodiyatisa@gmail.com>
2023-03-08 23:27:34 +0530

    second commit

--- style.js
+++ style.js
@@ -20,36 +20,8 @@ function maketable(){
 
 }
 document.getElementById('make').addEventListener("click",maketable);
-// // colorpicker
-// let colorinput=document.queryselector('#color');
-
-// colorinput.addEventListener(() =>{
-//     let color = colorinput.value;
-//  document.body.style.backgroundcolor = color;
-// });
-
-
-
-// const cols =parseInt( prompt("Enter number of columns:"));
-
-// const gridContainer = document.querySelector("#grid-container");
-
-// for(let j=0;j<cols;j++){
-//    gridContainer.style.gridTemplateColumns += " 1fr"
-// }
-// // create grid based on user input
-// for (let i = 0; i < cols*cols; i++) {
-   
-//   const gridItem = document.createElement("div");
-//   gridItem.classList.add("grid-item");
-//   gridItem.textContent = i + 1;
-
-//   gridContainer.appendChild(gridItem);
-// }
-
 
 // add color 
-const stack=[];
 
 let colorPicker=document.getElementById('colorPicker')
 let selectedColor;
@@ -58,25 +30,13 @@ colorPicker.addEventListener('change',(e) =>{
 
 } )
 //hover effect
-let item=document.querySelectorAll(".grid-item")
-item.forEach(function(x,i){
+let item=document.querySelectorAll(".td")
+console.log("fffff".td)
+item.forEach(function(x){
   x.addEventListener("mouseover",(e)=>{
-     //black color logic
-    // console.log(stack,"st1 ")
-    if(stack.length>9){
-        let j=0;
-        let itemToeDarken;
-        while(j<9){
-          itemToeDarken=stack.shift();
-          console.log(itemToeDarken,"st222")
-          itemToeDarken.style.backgroundColor="black";
-          j++
-        }
-      }
-      if(selectedColor){
+    
         e.target.style.backgroundColor= selectedColor;
-      stack.push(x)
-      }
+      
       
     })
   })
@@ -84,14 +44,14 @@ item.forEach(function(x,i){
   // onclick  eraser
   
   
-    function clear()
-   let eraser =document.getElementById("eraser")
-   let eitem=document.querySelectorAll(".grid-item")
-   eeraseritem.forEach(function (e){
-     e.addEventListener("click",(i)=>{
-      i.target.style.backgroundColor="white"
+  //   function clear()
+  //  let eraser =document.getElementById("eraser")
+  //  let eitem=document.querySelectorAll(".grid-item")
+  //  eeraseritem.forEach(function (e){
+  //    e.addEventListener("click",(i)=>{
+  //     i.target.style.backgroundColor="white"
   
-    })
-  })
+  //   })
+  // })
   
-  clear();
+  // clear();
